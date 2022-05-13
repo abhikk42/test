306bf41aee9490ad551613dbe03b264eabf9ad87
Abhikaushik21 <abhikaushik573@gmail.com>
2022-05-13 09:21:57 +0530

    first-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,416 @@
+<template>
+  <div class="headContainer">
+    <div class="firstHeader">
+      <div class="headLeft">
+        <h5>COMPLIMENTARY GIFT BOX PACKING ON ALL PRE-PAID ORDERS</h5>
+      </div>
+      <div class="headRight">
+        <div class="list">
+          <h5>Our Stores</h5>
+        </div>
+        <div class="list">
+          <h5>My Account</h5>
+        </div>
+        <div class="list">
+          <h5>Blog</h5>
+        </div>
+      </div>
+    </div>
+    <div class="midHeader">
+      <div class="leftList">
+        <div class="list">
+          <h2>Women</h2>
+          <img src="@/assets/down.svg" />
+        </div>
+        <div class="list">
+          <h2>Men</h2>
+          <img src="@/assets/down.svg" />
+        </div>
+        <div class="list"><h2>Sale</h2></div>
+        <div class="list">
+          <h2>Collection</h2>
+          <img src="@/assets/down.svg" />
+        </div>
+      </div>
+
+      <div class="list logo">
+        <font-awesome-icon
+          icon="bars"
+          class="responsivetoggler"
+          @click="handleMobileMenu()"
+        ></font-awesome-icon>
+        <img src="@/assets/footerLogo.svg/" class="logoimg1" />
+        <img src="@/assets/logonew.svg" class="logoimg" />
+      </div>
+      <div class="rightList">
+        <div class="list">
+          <h2>Our Stories</h2>
+          <!-- <img src="@/assets/down.svg"/> -->
+        </div>
+        <div class="list"><h2>CRAFTMANSHIP</h2></div>
+
+        <div class="icons">
+          <img src="@/assets/bag.svg" class="selicon" />
+
+          <img src="@/assets/like.svg" class="selicon" />
+          <img src="@/assets/search.svg" class="selicon" />
+        </div>
+      </div>
+    </div>
+    <div
+      class="mobilemenucontainer"
+      v-if="mobilemenu"
+      @click="mobilemenu = false"
+    >
+      <div class="mobilemenu">
+        <div class="mobilemenuhead">
+          <h4 :style="{ color: 'white' }">MENU</h4>
+          <button class="btncross" v-on:click="handleMobileMenu()">
+            <font-awesome-icon icon="fa-x" class="cross" />
+          </button>
+        </div>
+
+        <div class="list">
+          <h2>Women</h2>
+          <img src="@/assets/down.svg" />
+        </div>
+        <div class="list">
+          <h2>Men</h2>
+          <img src="@/assets/down.svg" />
+        </div>
+        <div class="list"><h2>Sale</h2></div>
+        <div class="list">
+          <h2>Collection</h2>
+          <img src="@/assets/down.svg" />
+        </div>
+
+        <div class="list">
+          <h2>Our Stories</h2>
+          <img src="@/assets/down.svg" />
+        </div>
+        <div class="list"><h2>CRAFTMANSHIP</h2></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header-comp",
+
+  data() {
+    return {
+      list: ["Women ", "Women", "Sale", "Collection"],
+      rightList: ["OUR STORY", "CRAFTMANSHIP"],
+      mobilemenu: false,
+      openFilter: true,
+    };
+  },
+  methods: {
+    handleMobileMenu() {
+      this.mobilemenu = this.mobilemenu == true ? false : true;
+      //  if(this.mobilemenu)
+      //  {
+      //    document.getElementById('mobmenu').style.left='0';
+      //  }else
+      //  {
+      //           document.getElementById('mobmenu').style.left='-100';
+
+      //  }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.headContainer {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  overflow: hidden;
+  align-items: center;
+  box-shadow: 0px 3px 5px #dfdfdf66;
+  font-family: "Jost";
+  width: 100%;
+}
+
+.midHeader {
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+}
+
+.firstHeader {
+  display: inline-flex;
+  width: 100%;
+  padding: 9px 0px;
+  align-items: center;
+  border-top: none;
+  border-bottom: 1px solid rgb(240, 240, 240);
+  height: 22px;
+
+  color: #303030;
+  border-bottom-width: thin;
+}
+.firstHeader > .headLeft,
+.firstHeader > .headRight {
+  width: 50%;
+  height: 100%;
+  align-items: center;
+  display: inline-flex;
+}
+
+.firstHeader > .headLeft {
+  justify-content: space-between;
+  margin: 0px 10px;
+}
+.headLeft > h5,
+.headRight > .list > h5 {
+  font-weight: 400;
+  font-size: 14px;
+  text-transform: uppercase;
+}
+.firstHeader > .headRight {
+  justify-content: flex-end;
+}
+.headRight > .list {
+  text-align: center;
+  width: 15%;
+}
+
+/*------------------MidHeader css--------------------*/
+
+.midHeader {
+  height: auto;
+  padding: 24.41px 0px;
+  color: #303030;
+}
+
+.leftList,
+.rightList {
+  display: inline-flex;
+  width: 40%;
+}
+.leftList {
+  justify-content: center;
+}
+
+.list {
+  display: inline-flex;
+  justify-content: center;
+  width: 20%;
+}
+.list > h2 {
+  font-weight: 500;
+  font-size: 16px;
+  text-transform: uppercase;
+}
+.list > img,
+.rightList > img {
+  vertical-align: middle;
+  margin: 0px 5px;
+  font-size: 14px;
+}
+.logoimg1 {
+  display: none;
+}
+.logo {
+  display: inline-flex;
+  width: 20%;
+
+  justify-content: center;
+  align-items: center;
+}
+.logo > .logoimg {
+  width: 166px;
+  height: 104px;
+}
+.rightList > .list {
+  justify-content: center;
+  flex: 1;
+
+  margin: 0px auto;
+}
+.responsivetoggler {
+  display: none;
+}
+.rightList > .icons {
+  flex: 2;
+  margin: 0px auto;
+}
+
+.selicon {
+  height: 25px;
+  float: right;
+  margin: 0px 20px;
+}
+.mobilemenu {
+  display: none;
+}
+@media (max-width: 768px) {
+  .headContainer {
+    max-width: 100vw;
+  }
+  .mobilemenuhead {
+    background: #501337;
+    display: flex;
+    align-items: center;
+    padding: 0px 10px;
+    justify-content: space-between;
+    width: 100%;
+    height: 10%;
+  }
+  .btncross {
+    background: none;
+    border: none;
+    color: white;
+  }
+  .btncross > .cross {
+    font-size: 15px;
+  }
+  .mobilemenucontainer {
+    display: flex;
+    width: 100%;
+    position: fixed;
+
+    top: 0;
+    left: -100;
+    background: rgba(0, 0, 0, 0.6);
+    z-index: 1;
+    height: 100%;
+    flex-direction: column;
+    transition-delay: left 10s linear;
+  }
+
+  .mobilemenu {
+    display: flex;
+    width: 70%;
+    position: fixed;
+    top: 0;
+    background: white;
+    z-index: 100;
+    height: 100%;
+    flex-direction: column;
+    transition-delay: left 10s linear;
+  }
+  .mobilemenu > .list {
+    width: 100%;
+    display: inline-flex;
+    justify-content: space-between;
+    margin: 0px auto;
+    align-items: center;
+    padding: 18px 10%;
+  }
+  .midHeader {
+    display: flex;
+    width: 100%;
+  }
+  .logoimg {
+    display: none;
+  }
+  .logo {
+    width: 50%;
+    justify-content: flex-start;
+    height: 10%;
+  }
+  .logoimg1 {
+    display: block;
+  }
+  .midHeader .leftList,
+  .firstHeader {
+    display: none;
+  }
+  .rightList {
+    width: 50%;
+  }
+  .sidepanel {
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    height: 250px;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    transition: 0.5s;
+
+    padding-top: 60px;
+  }
+
+  .sidepanel a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+  }
+
+  .sidepanel a:hover {
+    color: #f1f1f1;
+  }
+  .responsivetoggler {
+    display: block;
+    padding: 0px 20px;
+  }
+  .sidepanel .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+  }
+  .selicon {
+    margin: 0px 15px;
+    height: 90%;
+  }
+  .rightList > .list {
+    display: none;
+  }
+  .rightList > .icons {
+    flex: 1;
+    margin: 0px;
+  }
+
+  .openbtn {
+    font-size: 20px;
+    cursor: pointer;
+    background-color: #111;
+    color: white;
+    padding: 10px 15px;
+    border: none;
+  }
+  .logoimg1 {
+    padding: auto 10px;
+    height: 50%;
+  }
+
+  .openbtn:hover {
+    background-color: #444;
+  }
+}
+@media (min-width: 769px) and (max-width: 1024px) {
+  .list > h2 {
+    font-size: 12px;
+  }
+  .icons > .selicon {
+    font-size: 10px;
+    margin: 0px 10px;
+  }
+  .logo > .logoimg {
+    width: 83px;
+    height: 52px;
+  }
+  .headLeft > h5 {
+    font-size: 10px;
+  }
+  .headRight > .list > h5 {
+    font-size: 10px;
+  }
+}
+@media (max-width: 300px) {
+  .icons > .selicon {
+    font-size: 4px;
+    margin: 0px 4px;
+  }
+}
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,365 @@
+<template>
+  <div class="footerContainer">
+    <div class="footerHeader">
+      <img src="@/assets/leftLine.png/" class="img1" />
+
+      <img src="@/assets/footerLogo.svg/" class="img2" />
+      <img src="@/assets/rightLine.png/" class="img3" />
+    </div>
+    <div class="footerInnerContainer">
+      <div class="footerFirstCol">
+        <div class="footerItems">
+          <button class="footeraccordian" @click="handleFooterDrop('shop')">
+            SHOP
+            <span class="icon"
+              ><font-awesome-icon
+                :icon="data['shop'] == false ? 'fa-plus' : 'fa-minus'"
+              />
+            </span>
+          </button>
+
+          <ul v-if="data['shop']" class="responsiveul">
+            <li v-for="val in shop" :key="val">
+              <a class="link" href="#">{{ val }}</a>
+            </li>
+          </ul>
+
+          <ul class="footerul">
+            <h6>SHOP</h6>
+
+            <li v-for="val in shop" :key="val">
+              <a class="link" href="#">{{ val }}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footerItems">
+          <button class="footeraccordian" @click="handleFooterDrop('links')">
+            Quick Links
+            <span class="icon"
+              ><font-awesome-icon
+                :icon="data['links'] == false ? 'fa-plus' : 'fa-minus'"
+              />
+            </span>
+          </button>
+
+          <ul v-if="data['links']" class="responsiveul">
+            <li v-for="val in links" :key="val">
+              <a class="link" href="#">{{ val }}</a>
+            </li>
+          </ul>
+          <ul class="footerul">
+            <h6>QUICK LINKS</h6>
+            <li v-for="val in links" :key="val">
+              <a class="link" href="#">{{ val }}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footerItems">
+          <button class="footeraccordian" @click="handleFooterDrop('service')">
+            CUSTOMER SERVICE
+            <span class="icon"
+              ><font-awesome-icon
+                :icon="data['service'] == false ? 'fa-plus' : 'fa-minus'"
+              />
+            </span>
+          </button>
+
+          <ul v-if="data['service']" class="responsiveul">
+            <li v-for="val in services" :key="val">
+              <a class="link" href="#">{{ val }}</a>
+            </li>
+          </ul>
+          <ul class="footerul">
+            <h6>CUSTOMER SERVICE</h6>
+            <li v-for="val in services" :key="val">
+              <a class="link" href="#">{{ val }}</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="footerItems">
+          <button class="footeraccordian" @click="handleFooterDrop('profile')">
+            My Profile
+            <span class="icon"
+              ><font-awesome-icon
+                :icon="data['service'] == false ? 'fa-plus' : 'fa-minus'"
+              />
+            </span>
+          </button>
+          <ul v-if="data['profile']" class="responsiveul">
+            <li v-for="val in profile" :key="val">
+              <a class="link" href="#">{{ val }}</a>
+            </li>
+          </ul>
+          <ul class="footerul">
+            <h6>MY Profile</h6>
+
+            <li v-for="val in profile" :key="val">
+              <a class="link" href="#">{{ val }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="footerSecondCol">
+        <div class="footerItems">
+          <ul>
+            <h6>Sign Up and save</h6>
+            <li>
+              Subscribe to our newsletter and get 10% off your first purchase
+            </li>
+            <li class="input">
+              <input
+                type="text"
+                id="email"
+                placeholder="Enter your Email"
+              /><label for="email">Subscribe</label>
+            </li>
+            <li class="iconshead"><h5>GIVE US A FOLLOW</h5></li>
+            <div class="icons">
+              <a href="#"
+                ><img class="" src="@/assets/Group 80.png" alt=""
+              /></a>
+              <a href="#"><img src="@/assets/twitter.png" alt="" /></a>
+              <a href="#"><img src="@/assets/youtube.png" alt="" /></a>
+              <a href="#"><img src="@/assets/pinterest.png" alt="" /></a>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footerText">
+      Copyright © 2021 by Ahujasons I Powered by GreenHonchos
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer-comp",
+  data() {
+    return {
+      shop: ["Men", "Woman", "Home", "Sale", " Collection"],
+      services: [
+        "Terms and Condition",
+        "Shipping & Delivery",
+        " Returns & Cancellation",
+        " Contact us Store appointment",
+      ],
+      links: [
+        "Ahujasons Export",
+        "Ahujasons Wholesale",
+        "Our Story",
+        "Legacy",
+        " Craftsman ship Our Store",
+        "Blogs",
+      ],
+      profile: [
+        "My Account",
+        "Track Order",
+        "My Cart",
+        "WishList",
+        "Order Summary",
+      ],
+      signup: [],
+      ind: 0,
+      data: { shop: false, service: false, links: false, profile: "" },
+    };
+  },
+  methods: {
+    handleFooterDrop(value) {
+      this.data[value] = this.data[value] == false ? true : false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.footerInnerContainer {
+  display: grid;
+  overflow: hidden;
+
+  grid-template-columns: 70% 25%;
+  font-family: "Jost", sans-serif;
+
+  width: 100%;
+}
+.footerHeader {
+  display: flex;
+  align-items: center;
+  padding: 40px 22.6px;
+
+  width: 100%;
+}
+.footerHeader > .img1 {
+  justify-content: flex-start;
+  width: 48%;
+}
+.footeraccordian {
+  display: none;
+}
+.footerHeader > .img2 {
+  justify-content: center;
+  width: 92px;
+}
+.footerHeader > .img3 {
+  width: 48%;
+}
+.footerFirstCol {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  width: 100%;
+}
+ul {
+  list-style-type: none;
+  margin: 0px;
+  opacity: 0.8;
+}
+ul > h6 {
+  font-size: 16px;
+  font-family: "Jost", sans-serif;
+  text-transform: uppercase;
+
+  font-weight: 400;
+}
+.input {
+  outline: 0;
+  width: 100%;
+  display: flex;
+  border-bottom: 1px solid black;
+
+  justify-content: space-between;
+  width: 100%;
+  border-color: #000000;
+}
+input {
+  border: none;
+  width: calc(100% - 100px);
+}
+label {
+  font-weight: 600;
+}
+
+li,
+.sign-text {
+  font-size: 14px;
+  margin: 5% 0px;
+}
+.footerText {
+  display: inline-flex;
+  width: 95%;
+  font-size: 14px;
+  margin-top: 26px;
+
+  margin-bottom: 26px;
+
+  justify-content: flex-end;
+  align-items: center;
+}
+.icon {
+  float: right;
+  font-size: 16px;
+}
+.responsiveul {
+  display: none;
+}
+.footerul {
+  display: block;
+}
+
+.link {
+  color: #303030;
+  text-decoration: none;
+}
+.icons > a {
+  margin: 0 5%;
+  height: 15%;
+}
+@media (max-width: 768px) {
+  .footerHeader {
+    margin: 0px;
+  }
+  .footerInnerContainer {
+    display: flex;
+    flex-direction: column;
+    padding: 0px;
+    width: 100%;
+  }
+  .footerHeader > .img1,
+  .footerHeader > .img3 {
+    width: 40%;
+  }
+  .footerHeader > .img2 {
+    width: 20%;
+  }
+  .footerFirstCol {
+    order: 2;
+    flex-direction: column;
+    display: flex;
+    width: 100%;
+  }
+  .footerItems {
+    flex-basis: 95%;
+    border-bottom: 1px solid #707070;
+  }
+  .footerText {
+    flex-basis: 100%;
+    font-size: 16px;
+    display: flex;
+    padding: 0px 15px;
+    margin-bottom: 20%;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .footerSecondCol {
+    order: 1;
+    font-size: 18px;
+    width: 100%;
+  }
+  .footerItems > ul > h6 {
+    font-size: 20px;
+  }
+  .footerItems > ul > li {
+    font-size: 15px;
+  }
+  .responsiveul {
+    display: block;
+    font-size: 20px;
+  }
+  .responsiveul > li {
+    font-size: 15px;
+  }
+  .footerul {
+    display: none;
+  }
+  .footeraccordian {
+    background-color: white;
+    color: #444;
+    display: block;
+    cursor: pointer;
+    text-transform: uppercase;
+    padding: 18px 20px 18px 20px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    font-weight: 600;
+    outline: none;
+    font-size: 16px;
+    transition: 0.4s;
+  }
+  .icons {
+    padding: 20px 0px;
+  }
+
+  ul {
+    padding: 0px 20px;
+  }
+  .footerItems {
+    width: 100%;
+  }
+  .footerSecondCol {
+    order: 1;
+    width: 100%;
+  }
+}
+</style>
